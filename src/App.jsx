1d1565dd31c513381a3559199b97a228a1747c18
Jagadeshwaran P <148187521+jaga-Innovation@users.noreply.github.com>
2024-11-18 13:18:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import ControlledForm from "./ControlledForm";
+import UncontrolledForm from "./UncontrolledForm";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -22,6 +24,8 @@ function App() {
           count is {count}
         </button>
       </div>
+      <ControlledForm />
+      <UncontrolledForm />
     </>
   );
 }
